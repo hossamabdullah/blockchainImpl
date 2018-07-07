ddd8ed86f72bc6967cd247d84c0befd0f21b73aa
Hossameldeen <hossamabdalh@gmail.com>
2018-07-08 00:07:22 +0200

    better names

--- blockchain.py
+++ blockchain.py
@@ -16,7 +16,18 @@ class Blockchain:
     def __init__(self):
             self.chain = []
             self.create_block(proof = 1, previous_hash = '0')
-            
+        
+    def mine(self, previous_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = self._try_nonce(proof= new_proof, prev_proof= previous_proof)
+            if hash_operation[:4] == '0000':
+                check_proof = True
+                break
+            new_proof += 1
+        return new_proof
+    
     def create_block(self, proof, previous_hash):
         block = {'index': len(self.chain)+1,
                  'timestamp': str(datetime.datetime.now()),
@@ -28,27 +39,12 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
     
-    def proof_of_work(self, previous_proof):
-        new_proof = 1
-        check_proof = False
-        while check_proof is False:
-            hash_operation = self._try_nonce(proof= new_proof, prev_proof= previous_proof)
-            if hash_operation[:4] == '0000':
-                check_proof = True
-                break
-            new_proof += 1
-        return new_proof
-    
-    def hash(self, block):
-        encoded_block = json.dumps(block, sort_keys = True).encode()
-        return hashlib.sha256(encoded_block).hexdigest()
-    
     def is_chain_valid(self, chain):
         previous_block = chain[0]
         block_index = 1
         while block_index < len(chain):
             block = chain[block_index]
-            if block['previous_hash'] != self.hash(previous_block):
+            if block['previous_hash'] != self._hash(previous_block):
                 return False
             
             previous_proof = previous_block['proof']
@@ -60,6 +56,10 @@ class Blockchain:
             previous_block = block
             block_index += 1
         return True
+    
+    def _hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
     
     def _try_nonce(self, proof, prev_proof):
         return hashlib.sha256(str(proof**2 -  prev_proof**2).encode()).hexdigest()
@@ -74,8 +74,8 @@ blockchain = Blockchain()
 def mine_block():
     prev_block = blockchain.get_previous_block()
     prev_proof = prev_block['proof']
-    proof = blockchain.proof_of_work(previous_proof= prev_proof)
-    prev_block_hash = blockchain.hash(prev_block)
+    proof = blockchain.mine(previous_proof= prev_proof)
+    prev_block_hash = blockchain._hash(prev_block)
     block = blockchain.create_block(proof=proof , previous_hash=prev_block_hash)
     response = {'message': 'Congratulations, you just mined a block!',
                 'index': block['index'],
@@ -91,5 +91,10 @@ def get_chain():
                 'length': len(blockchain.chain)}    
     return jsonify(response), 200
 
+
+@app.route('/is_valid', method=['GET'])
+def is_valid():
+    response = {'is_valid': blockchain.is_chain_valid()}
+    return jsonify(response), 200
 
 app.run(host= '0.0.0.0', port= 5000)
