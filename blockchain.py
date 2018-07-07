5c8021e64bd2c8a18ba0bcd03023ac9df6f18fea
Hossameldeen <hossamabdalh@gmail.com>
2018-07-07 23:32:17 +0200

    step 11

--- blockchain.py
+++ blockchain.py
@@ -85,35 +85,12 @@ def mine_block():
     return jsonify(response), 200
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    
+    return jsonify(response), 200
 
 
 
