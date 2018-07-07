b4a4351195bf2d32c3b10efd9d76dad26244b61e
Hossameldeen <hossamabdalh@gmail.com>
2018-07-07 22:58:24 +0200

    step 7

--- blockchain.py
+++ blockchain.py
@@ -31,15 +31,17 @@ class Blockchain:
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
-        
         while check_proof is False:
             hash_operation = hashlib.sha256(str(new_proof**2 -  previous_proof**2).encode())
             if hash_operation[:4] == '0000':
                 check_proof = True
                 break
             new_proof += 1
-        
         return new_proof
+    
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
             
         
 #part 2 - Mining our Blockchain
