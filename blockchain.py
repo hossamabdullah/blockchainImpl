83e9e5cda5a16ab0c3e5897383479aed00309e6a
Hossameldeen <hossamabdalh@gmail.com>
2018-07-07 22:34:57 +0200

    step 4

--- blockchain.py
+++ blockchain.py
@@ -16,4 +16,12 @@ class Blockchain:
     def __init__(self):
             self.chain = []
             self.create_block(proof = 1, previoud_hash = '0')
+            
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        self.chain.append(block)
+        return block
 #part 2 - Mining our Blockchain
